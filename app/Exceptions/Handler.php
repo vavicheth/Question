a8523e3ec1e60cfe80a6173bbedab60afc2022c0
vavicheth <vicheth_it@yahoo.com>
2020-02-17 11:24:43 +0700

    JWT Middleware and Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +55,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof TokenBlacklistedException){
+            return response(['error'=>'Token is blacklist'],Response::HTTP_BAD_REQUEST);
+        }
+        else if($exception instanceof TokenInvalidException){
+            return response(['error'=>'Token is not valid'],Response::HTTP_BAD_REQUEST);
+        }
+        else if($exception instanceof TokenExpiredException){
+            return response(['error'=>'Token is expired'],Response::HTTP_BAD_REQUEST);
+        }
+        else if($exception instanceof JWTException){
+            return response(['error'=>'Token is not provided'],Response::HTTP_BAD_REQUEST);
+        }
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('JWT', ['except' => ['login','signup']]);
     }
 
     /**
